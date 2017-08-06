6196604ee746e5dc5e6fa0caa977009079209e6f
Akhil Nair <akhil.nair@amey.co.uk>
2017-08-06 21:58:30 +0100

    :-1: Added filepath to config

--- munge/02_load_data.r
+++ munge/02_load_data.r
@@ -4,6 +4,7 @@ url_data = readLines("data/house_price.url")
 flag_download = config$download
 years = config$years
 colnames = config$data_specification$dt_house_prices$colnames
+filepath = config$data_specification$dt_house_prices$save_as
 # Build all s3 links
 urls = lapply(years, function(year) url_data %format% list(year = year))
 
@@ -13,10 +14,10 @@ if (flag_download) {
   # Rename columns
   data.table::setnames(dt, names(colnames), unlist(colnames, use.names = FALSE))
   # Write to cache for easy loading next time
-  fst::write.fst(dt, "cache/house_prices.fst")
+  fst::write.fst(dt, filepath)
   message("Data written to 'cache/'")
-} else if (file.exists("cache/house_prices.fst")) {
-  dt = fst::read.fst("cache/house_prices.fst", as.data.table = TRUE)
+} else if (file.exists(filepath)) {
+  dt = fst::read.fst(filepath, as.data.table = TRUE)
   message("Data read from 'cache/'")
 } else {
   message("Initialise data in 'cache/' by changing download flag in 'conf/conf.yml' to TRUE")
