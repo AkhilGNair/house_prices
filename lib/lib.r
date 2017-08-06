85e9cd2f3869c39a147961814fd273885977e315
Akhil Nair <akhil.nair@amey.co.uk>
2017-08-06 21:30:12 +0100

    :house: Initial commit

---
+++ lib/lib.r
@@ -0,0 +1,17 @@
+#' Infix to format strings via named paramters
+#'
+#' @param fmt A string to format
+#' @param list A list of named parameters
+#'
+#' @examples
+#' parameters <- list(label = "months", april = 4, may = 5, june = 6)
+#' "%(label)s: %(april)d %(may)d %(june)d" %format% parameters
+#'
+#' @export
+`%format%` <- function(fmt, list) {
+  pat <- "%\\(([^)]*)\\)"
+  pat2 <- "(?<=%\\()([^)]*)(?=\\))"
+  fmt2 <- gsub(pat, "%", fmt)
+  list2 <- list[stringr::str_extract_all(fmt, pat2)[[1]]]
+  do.call("sprintf", c(fmt2, list2))
+}
---
+++ script.r
@@ -0,0 +1,22 @@
+source("lib/lib.r")
+library(data.table)
+library(magrittr)
+
+config = config::get(file = "conf/conf.yml")
+url_data = readLines("data/house_price.url")
+
+# To initialise, change download flag in "conf/conf.yml" to TRUE
+flag_download = config$download
+years = config$years
+colnames = config$data_specification$dt_house_prices$colnames
+# Build all s3 links
+urls = lapply(years, function(year) url_data %format% list(year = year))
+
+if (flag_download) {
+  # Read all s3 files and bind into one dataset
+  dt = lapply(urls, fread, header = FALSE) %>% rbindlist()
+  # Rename columns
+  data.table::setnames(dt, names(colnames), unlist(colnames, use.names = FALSE))
+  # Write to cache for easy loading next time
+  fst::write.fst(dt, "cache/house_prices.fst")
+}
